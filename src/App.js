a94eb0458e7a03002bb68f1caa0e8d6efc5d6ece
Noe Schultz <noeschultz96@gmail.com>
2019-04-10 14:03:18 -0400

    added ImageLinkForm and Logo components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import Navigation from './Components/Navigation/Navigation';
+import Particles from 'react-particles-js';
 import Logo from './Components/Logo/Logo';
+import Rank from './Components/Rank/Rank';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import './App.css';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 300,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Particles className="particles"
+                params={particlesOptions} />
         <Navigation />
         <Logo /> 
-        {/*<ImageLinkForm />
-        <FaceRecognition />*/}
+        <Rank />
+        <ImageLinkForm />
+        {/*<FaceRecognition />*/}
       </div>
     );
   }
--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Tilt from 'react-tilt'; 
+import face from './face.png';
+import './Logo.css';
 
 const Logo = () => {
     return (
         <div className="ma4 mt0">
-            <p>Logo</p>
+            <Tilt options={{ max : 65 }} style={{ height: 250, width: 150 }} >
+                <div className="Tilt-inner"><img style={{paddingTop:'5px', height:'200px',width: '150px'}} src={face} alt="logo"/></div>
+            </Tilt>
         </div>
     );
 }
