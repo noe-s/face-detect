78b0921c7e6ed3a3adc24675ad077c90a750e82a
Noe Schultz <noeschultz96@gmail.com>
2019-04-15 10:33:22 -0400

    edited ImageLink, and added signin/register capabilities

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import Navigation from './Components/Navigation/Navigation';
+import SignIn from './Components/SignIn/SignIn';
+import Register from './Components/Register/Register';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Logo from './Components/Logo/Logo';
 import Rank from './Components/Rank/Rank';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: '90eb62b6379f4f8faedc291815e0d843'
+ });
 
 const particlesOptions = {
   particles: {
@@ -19,16 +27,66 @@ const particlesOptions = {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false
+    }
+  }
+
+  onInputChange = (e) => {
+    this.setState({input: e.target.value});
+  };
+  
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input })
+      app.models.predict(
+        Clarifai.COLOR_MODEL, 
+        this.state.input).then(
+      function(response) {
+        console.log(response);
+      },
+      function(err) {
+        console.log(err);
+      }
+    );
+  };
+
+  onRouteChange = (route) => {
+    if(route ==='signout') {
+      this.setState({isSignedIn: false})
+    } else if(route ==='home') {
+      this.setState({route: route });
+    }
+  }
+
+  isSignedIn = () => {
+    console.log('signin')
+  }
+
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-      <Particles className="particles"
-                params={particlesOptions} />
-        <Navigation />
-        <Logo /> 
-        <Rank />
-        <ImageLinkForm />
-        {/*<FaceRecognition />*/}
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        {this.state.route === 'home' //if state is signin, load these components
+          ? <div>     
+              <Logo /> 
+              <Rank />
+              <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+              <FaceRecognition imageUrl={imageUrl}/>
+            </div>
+          : (
+            this.state.route === 'signin'
+            ? <SignIn onRouteChange={this.onRouteChange}/>
+            : <Register onRouteChange={this.onRouteChange} />
+          )
+        }
       </div>
     );
   }
